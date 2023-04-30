35d80e415b059dd5e68858cc77aff45b04e27f86
BradenStitt <stittbraden@gmail.com>
2023-04-30 12:50:58 -0700

    Added post deployment script with hardcoded values for hackathon data into the SQL database.

--- DevpostDB/Script.PostDeployment.sql
+++ DevpostDB/Script.PostDeployment.sql
@@ -1,11 +1,9 @@
-﻿/*
-Post-Deployment Script Template							
---------------------------------------------------------------------------------------
- This file contains SQL statements that will be appended to the build script.		
- Use SQLCMD syntax to include a file in the post-deployment script.			
- Example:      :r .\myfile.sql								
- Use SQLCMD syntax to reference a variable in the post-deployment script.		
- Example:      :setvar TableName MyTable							
-               SELECT * FROM [$(TableName)]					
---------------------------------------------------------------------------------------
-*/
+﻿if not exists (select 1 from dbo.Hackathons)
+begin
+	insert into dbo.Hackathons (HackathonName, HackathonLocation, HackathonStartDate, HackathonEndDate, HackathonURL)
+	VALUES ('Hack the Crisis Nigeria 2022', 'Lagos, Nigeria', '2022-08-15', '2022-08-20', 'https://www.devpost.com/hackthecrisisnigeria2022'),
+	   ('Hack the North 2022', 'Waterloo, Canada', '2022-09-16', '2022-09-18', 'https://www.devpost.com/hackthenorth2022'),
+	   ('HackCU VIII', 'Boulder, USA', '2022-02-25', '2022-02-27', 'https://www.devpost.com/hackcu-viii'),
+	   ('TechTogether Boston 2022', 'Boston, USA', '2022-04-01', '2022-04-03', 'https://www.devpost.com/techtogetherboston2022'),
+	   ('HACK IT OUT 2022', 'Singapore', '2022-06-10', '2022-06-12', 'https://www.devpost.com/hackitout2022');
+end
