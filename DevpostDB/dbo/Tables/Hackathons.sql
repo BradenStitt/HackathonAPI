4c621ead6849c2946393a6ce1a6d793e24a2e4d0
BradenStitt <stittbraden@gmail.com>
2023-04-30 12:19:51 -0700

    Added SQL Server database creation script to store Devpost API data.

---
+++ DevpostDB/dbo/Tables/Hackathons.sql
@@ -0,0 +1,9 @@
+﻿CREATE TABLE [dbo].[Hackathons]
+(
+	[Id] INT NOT NULL PRIMARY KEY IDENTITY, 
+    [HackathonName] NVARCHAR(255) NOT NULL,
+	[Location] NVARCHAR(255),
+    [StartDate] DATE,
+    [EndDate] DATE,
+    [URL] NVARCHAR(255)
+)
